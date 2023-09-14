433a78b92e0941dfe93157de482d4daccf3c06d2
chunyuan-w <chunyuan.wu@intel.com>
2023-09-14 09:32:27 +0000

    simplify UT to directly return

--- python/tutorials/01-vector-add.py
+++ python/tutorials/01-vector-add.py
@@ -17,7 +17,7 @@ In doing so, you will learn about:
 # %%
 # Compute Kernel
 # --------------
-
+import copy
 import torch
 
 import triton
@@ -27,30 +27,29 @@ import triton.language as tl
 @triton.jit
 def add_kernel(
     x_ptr,  # *Pointer* to first input vector.
-    y_ptr,  # *Pointer* to second input vector.
-    output_ptr,  # *Pointer* to output vector.
     n_elements,  # Size of the vector.
     BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
                  # NOTE: `constexpr` so it can be used as a shape value.
 ):
-    # There are multiple 'programs' processing different data. We identify which program
-    # we are here:
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
-    # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers:
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses.
-    mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size.
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    # Write x + y back to DRAM.
-    tl.store(output_ptr + offsets, output, mask=mask)
+    # # There are multiple 'programs' processing different data. We identify which program
+    # # we are here:
+    # pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # # This program will process inputs that are offset from the initial data.
+    # # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # # Note that offsets is a list of pointers:
+    # block_start = pid * BLOCK_SIZE
+    # offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # # Create a mask to guard memory operations against out-of-bounds accesses.
+    # mask = offsets < n_elements
+    # # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # # multiple of the block size.
+    # x = tl.load(x_ptr + offsets, mask=mask)
+    # y = tl.load(y_ptr + offsets, mask=mask)
+    # output = x + y
+    # # Write x + y back to DRAM.
+    # tl.store(output_ptr + offsets, output, mask=mask)
+    return
 
 
 # %%
@@ -60,7 +59,8 @@ def add_kernel(
 
 def add(x: torch.Tensor, y: torch.Tensor):
     # We need to preallocate the output.
-    output = torch.empty_like(x)
+    # output = torch.empty_like(x)
+    output = torch.zeros_like(x)
     # assert x.is_cuda and y.is_cuda and output.is_cuda
     n_elements = output.numel()
     # The SPMD launch grid denotes the number of kernel instances that run in parallel.
@@ -71,10 +71,10 @@ def add(x: torch.Tensor, y: torch.Tensor):
     #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
     #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
     #  - Don't forget to pass meta-parameters as keywords arguments.
-    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    add_kernel[grid](x, n_elements, BLOCK_SIZE=1024)
     # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
     # running asynchronously at this point.
-    return output
+    return x
 
 
 # %%
@@ -85,14 +85,15 @@ device = 'cpu'
 torch.manual_seed(0)
 size = 98432
 x = torch.rand(size, device=device)
+x_ref = copy.deepcopy(x)
 y = torch.rand(size, device=device)
 output_torch = x + y
 output_triton = add(x, y)
-print(output_torch)
+print(x_ref)
 print(output_triton)
 print(
     f'The maximum difference between torch and triton is '
-    f'{torch.max(torch.abs(output_torch - output_triton))}'
+    f'{torch.max(torch.abs(x_ref - output_triton))}'
 )
 
 # %%
